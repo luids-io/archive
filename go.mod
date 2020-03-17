1684c69584b15ce153438e4965453a0d122f1957
Luis Guillen Civera <luisguillenc@gmail.com>
2020-03-17 09:32:30 +0100

    updated to new dnsutil.ResolvData

--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.13
 
 require (
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
-	github.com/gofrs/uuid v3.2.0+incompatible
+	github.com/gofrs/uuid v3.2.0+incompatible // indirect
 	github.com/google/gopacket v1.1.17 // indirect
 	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/luids-io/api v0.0.0-20200228121542-0ac53a4f06b8
 	github.com/luids-io/common v0.0.0-20200210131732-cafa5e19caa9
-	github.com/luids-io/core v0.0.0-20200228120756-a29a5399715a
+	github.com/luids-io/core v0.0.0-20200317081533-8d76b8a5f5e6
 	github.com/luisguillenc/goconfig v0.0.0-20191024075953-57413864b1d6
 	github.com/luisguillenc/grpctls v0.0.0-20191021152419-e99d6682ee98 // indirect
 	github.com/luisguillenc/httphealth v0.0.0-20200210131312-e37786300489 // indirect
@@ -20,7 +20,7 @@ require (
 	github.com/luisguillenc/tlslayer v0.0.0-20191031133340-d503d3e478b7 // indirect
 	github.com/luisguillenc/yalogi v0.0.0-20190329093036-2b8db3a816d3
 	github.com/patrickmn/go-cache v2.1.0+incompatible
-	github.com/sirupsen/logrus v1.4.2
+	github.com/sirupsen/logrus v1.4.2 // indirect
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.2
 	google.golang.org/grpc v1.26.0
--- pkg/archive/service/dnsmdb/models.go
+++ pkg/archive/service/dnsmdb/models.go
@@ -7,25 +7,41 @@ import (
 )
 
 type mdbResolvData struct {
-	ID          string    `json:"id" bson:"_id"`
-	Timestamp   time.Time `json:"timestamp"`
-	ServerIP    string    `json:"server_ip"`
-	ClientIP    string    `json:"client_ip"`
-	ResolvedIPs []string  `json:"resolved_ips"`
-	Name        string    `json:"name"`
+	ID        string        `json:"id" bson:"_id"`
+	Timestamp time.Time     `json:"timestamp"`
+	Duration  time.Duration `json:"duration"`
+	ServerIP  string        `json:"server_ip"`
+	ClientIP  string        `json:"client_ip"`
+	//query info
+	QID              uint16 `json:"qid"`
+	Name             string `json:"name"`
+	CheckingDisabled bool   `json:"checking_disabled"`
+	//response info
+	ReturnCode        uint16   `json:"return_code"`
+	AuthenticatedData bool     `json:"authenticated_data"`
+	ResolvedIPs       []string `json:"resolved_ips,omitempty" bson:",omitempty"`
 }
 
 func toMongoData(data dnsutil.ResolvData) mdbResolvData {
-	resolved := make([]string, 0, len(data.Resolved))
-	for _, r := range data.Resolved {
-		resolved = append(resolved, r.String())
+	mdbData := mdbResolvData{
+		ID:        data.ID,
+		Timestamp: data.Timestamp,
+		Duration:  data.Duration,
+		ServerIP:  data.Server.String(),
+		ClientIP:  data.Client.String(),
+		//query data
+		QID:              data.QID,
+		Name:             data.Name,
+		CheckingDisabled: data.CheckingDisabled,
+		//response data
+		ReturnCode:        data.ReturnCode,
+		AuthenticatedData: data.AuthenticatedData,
 	}
-	return mdbResolvData{
-		ID:          data.ID,
-		Timestamp:   data.Timestamp,
-		ServerIP:    data.Server.String(),
-		ClientIP:    data.Client.String(),
-		ResolvedIPs: resolved,
-		Name:        data.Name,
+	if len(data.Resolved) > 0 {
+		mdbData.ResolvedIPs = make([]string, 0, len(data.Resolved))
+		for _, r := range data.Resolved {
+			mdbData.ResolvedIPs = append(mdbData.ResolvedIPs, r.String())
+		}
 	}
+	return mdbData
 }
