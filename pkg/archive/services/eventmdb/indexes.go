42f48c83f19640f57b3d8ab0cd7e402c1a1b8105
Luis Guillén Civera <luisguillenc@gmail.com>
2020-10-22 08:23:04 +0200

    eventmdb: add indexes

--- pkg/archive/services/eventmdb/indexes.go
+++ pkg/archive/services/eventmdb/indexes.go
@@ -2,6 +2,25 @@
 
 package eventmdb
 
+import "github.com/globalsign/mgo"
+
 func (a *Archiver) createIdx() error {
+	return a.createIdxEvents()
+}
+
+func (a *Archiver) createIdxEvents() error {
+	c := a.getCollection(EventColName)
+	indexes := []mgo.Index{
+		{Key: []string{"created"}},
+		{Key: []string{"code"}},
+		{Key: []string{"level"}},
+		{Key: []string{"$text:description"}},
+	}
+	for _, idx := range indexes {
+		err := c.EnsureIndex(idx)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
